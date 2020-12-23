b54f7cc178d78e22a95239e2aeae761a34539dc2
hcy13 <76154335+hcy13@users.noreply.github.com>
2020-12-23 23:16:00 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,727 @@
+<template>
+  <div class="hello">
+    <img class="back" src="https://i.imgur.com/5JDXt8M.jpg">
+    <div class="circle1"><br>藝術學院</div>
+    <div class="circle2"><br>HOME</div>
+    <div class="circle3"><br>NEWS</div>
+    <div class="circle4"><br>招 生</div>
+    <div class="circle6"><br>學士班</div>
+    <div class="circle7"><br>師資</div>
+    <div class="circle8"><br>科藝賞析</div>
+    <div class="circle9"><br>聯絡</div>
+
+    <h1>{{ msg }}</h1>
+    <div class="h5">I P T A</div>
+    <h2>學士班特色</h2>
+    <ul>
+      <p>
+        清華是國際頂尖科技大學，加上合校後的藝術學院，具備發展「科技藝術」的卓越條件。我們號召臺灣年輕一代的科技藝術學者專家，我們規劃有科技藝術跨域特別課程，並與業界合作推出創意跨域專題。你，不需猶豫，歡迎你的加入！
+      </p>
+    </ul>
+    <h3>核心能力</h3>
+    <u1>
+      <p>社會關懷與品格實踐能力｜美感認知與經典欣賞能力｜自然感知與創新發想能力｜邏輯思辨與語文溝通能力｜國際觀點與在地行動能力｜資訊掌握與科技應用能力</p>
+    </u1>
+    <u2>
+      <p>
+        清華大學2018年成立藝術學院〝科技藝術〞跨域「學士班」。「科技藝術（TechArt）」是當代藝術的新形式創作也是國際藝術發展的重要趨勢，包含虛擬藝術、互動藝術、電腦動畫、電腦音樂、聲音藝術、科技性表演藝術、混合藝術、網路社群、創客藝術、仿生藝術、生物藝術、人工生命…等。在當代數位美學與數位藝術的基礎下，我們結合創新科技，如各種互動感測技術、虛擬實境、物聯網、人工智慧、大數據分析、基因工程…，把「科技」與「藝術」結合視為「未來文化學」，培養前瞻性、跨域研究與創作人才。
+      </p>
+    </u2>
+    <u3>
+      <p>
+        因應未來產業的發展趨勢，創新科技與當代藝術結合之創作如雨後春筍。清華大學的是國內科學與科技的頂尖學府，對藝術學院發展「科技藝術」具有得天獨厚的優勢。本班師資除來自清華校內藝術學院、電資學院、工學院、生命科學院、人文社會等跨域教師外，我們也將聘進一群來自美國、歐洲與日本跨國與跨域的專業教師。也廣邀在臺灣在科技藝術創作領域最頂尖的業界團隊與藝術家加入我們的教學團隊，讓同學得到專業與多元的指導，並在共享學習、共享創意的環境裡，開創未來的產業，盡情發揮自己的潛能。
+      </p>
+    </u3>
+    <h4>院學士班最新訊息</h4>
+    <u4>
+      <p>【獲獎】《那顆Ｇ1咻！》國立清華大學藝術學院學士班第一屆系展 得獎名單</p>
+    </u4>
+    <u5>
+      <p>【獲獎】2020春之清華 – 藝術卓越獎 科技藝術類 得獎名單</p>
+    </u5>
+    <u6>
+      <p>【徵件公告】春之清華—藝術卓越獎科技藝術類 甄選辦法</p>
+    </u6>
+    <u7>
+      <p>《那顆Ｇ1咻！》國立清華大學藝術學院學士班第一屆系展</p>
+    </u7>
+    <u8>
+      <p>國立清華大學 藝術學院學士班 徵聘【助理教授級以上】專任教師1名</p>
+    </u8>
+    <u9>
+      <p>【獲獎】2020藝術學院學士班春之清華獎學金 獲獎名單</p>
+    </u9>
+    <u10>
+      <p>《 科技物件與人類幻象講座 Ⅸ》6/16 Tue</p>
+    </u10>
+    <u11>
+      <p>《 科技物件與人類幻象講座 Ⅶ&Ⅷ》5/26 & 6/2 Tue</p>
+    </u11>
+    <u12>
+      <p>【講座】05/26 當代劇場展演實務 華山藝文林璞總監</p>
+    </u12>
+    <u13>
+      <p>【演講】05/28 從腦科學看藝術 (Art and the Brain: A Glimpse of Neuroaesthetics)</p>
+    </u13>
+    <u14>
+      <p>【講座】虛擬藝術美學思維與創作實踐</p>
+    </u14>
+    
+
+    
+
+    <div class="circle1"><br>藝術學院</div>
+    <div class="circle2"><br>HOME</div>
+    <div class="circle3"><br>NEWS</div>
+    <div class="circle4"><br>招 生</div>
+    <div class="circle6"><br>學士班</div>
+    <div class="circle7"><br>師資</div>
+    <div class="circle8"><br>科藝賞析</div>
+    <div class="circle9"><br>聯絡</div>
+
+    <div class="circle5"><br><br>藝術學院</div>
+    <div class="circle10"><br><br>HOME</div>
+    <div class="circle11"><br><br>NEWS</div>
+    <div class="circle12"><br><br>招 生</div>
+    <div class="circle13"><br><br>學士班</div>
+    <div class="circle14"><br><br>師資</div>
+    <div class="circle15"><br><br>科藝賞析</div>
+    <div class="circle16"><br><br>聯絡</div>
+
+    <img class="img1" src="https://ipta.nthu.edu.tw/wp-content/uploads/2019/02/cropped-NTHU-logo.png">
+    <img class="img2" src="https://ipta.nthu.edu.tw/wp-content/uploads/2020/10/banner.png">
+    <img class="img3" src="https://ipta.nthu.edu.tw/wp-content/uploads/2020/09/light2-01.jpg">
+    <img class="img4" src="https://ipta.nthu.edu.tw/wp-content/uploads/2020/09/light2-01.jpg">
+    <img class="img5" src="https://ipta.nthu.edu.tw/wp-content/uploads/2020/10/banner.png">
+    <img class="img6" src="https://ipta.nthu.edu.tw/wp-content/uploads/2020/04/NEWS.jpg">
+    <img class="img7" src="https://ipta.nthu.edu.tw/wp-content/uploads/2019/10/board-2303325_1920.jpg">
+    <img class="img8" src="https://ipta.nthu.edu.tw/wp-content/uploads/2020/05/tfho.jpg">
+    <img class="img9" src="https://ipta.nthu.edu.tw/wp-content/uploads/2020/05/tfho.jpg">
+    <img class="img10" src="https://ipta.nthu.edu.tw/wp-content/uploads/2020/04/5e7987.jpg">
+    <img class="img11" src="https://ipta.nthu.edu.tw/wp-content/uploads/2020/05/200528.jpg">
+    <img class="img12" src="https://ipta.nthu.edu.tw/wp-content/uploads/2020/04/5e7987.jpg">
+
+    <div class="under"></div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '藝術學院學士班'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.circle1 {
+  z-index: 999;
+  border-radius:100%;
+  position: fixed;
+  width: 60px;
+  height: 60px;
+  right: 559px;
+  top: 20px;
+  border: solid 2px black;
+  text-align: center; 
+  font-size: 15px;
+}
+.circle2 {
+  z-index: 999;
+  border-radius:100%;
+  position: fixed;
+  width: 60px;
+  height: 60px;
+  right: 483px;
+  top: 20px;
+  border: solid 2px black;
+  
+}
+.circle3 {
+  z-index: 999;
+  border-radius:100%;
+  position: fixed;
+  width: 60px;
+  height: 60px;
+  right: 409px;
+  top: 20px;
+  border: solid 2px black;
+}
+.circle4 {
+  z-index: 999;
+  border-radius:100%;
+  position: fixed;
+  width: 60px;
+  height: 60px;
+  right: 335px;
+  top: 20px;
+  border: solid 2px black;
+  font-size: 15px;
+}
+
+.circle6 {
+  z-index: 999;
+  border-radius:100%;
+  position: fixed;
+  width: 60px;
+  height: 60px;
+  right: 260px;
+  top: 20px;
+  border: solid 2px black;
+  text-align: center;
+  font-size: 15px;
+}
+.circle7 {
+  z-index: 999;
+  border-radius:100%;
+  position: fixed;
+  width: 60px;
+  height: 60px;
+  right: 185px;
+  top: 20px;
+  border: solid 2px black;
+  font-size: 15px;
+}
+.circle8 {
+  z-index: 999;
+  border-radius:100%;
+  position: fixed;
+  width: 60px;
+  height: 60px;
+  right: 110px;
+  top: 20px;
+  border: solid 2px black;
+  font-size: 15px;
+}
+.circle9 {
+  z-index: 999;
+  border-radius:100%;
+  position: fixed;
+  width: 60px;
+  height: 60px;
+  right: 35px;
+  top: 20px;
+  border: solid 2px black;
+  font-size: 15px;
+}
+.circle5 {
+  border-radius:100%;
+  position: absolute;
+  width: 205px;
+  height: 200px;
+  left: 252px;
+  top: 1324px;
+  border: solid 2px black;
+  font-size: 30px;
+}
+.circle10 {
+  border-radius:100%;
+  position: absolute;
+  width: 200px;
+  height: 200px;
+  left: 522px;
+  top: 1324px;
+  border: solid 2px black;
+  font-size: 30px;
+}
+.circle11 {
+  border-radius:100%;
+  position: absolute;
+  width: 200px;
+  height: 200px;
+  left: 786px;
+  top: 1324px;
+  border: solid 2px black;
+  font-size: 30px;
+  
+}
+.circle12 {
+  border-radius:100%;
+  position: absolute;
+  width: 200px;
+  height: 200px;
+  left: 1053px;
+  top: 1324px;
+  border: solid 2px black;
+  font-size: 30px;
+}
+.circle13 {
+  border-radius:100%;
+  position: absolute;
+  width: 200px;
+  height: 200px;
+  left: 255px;
+  top: 1565px;
+  border: solid 2px black;
+  font-size: 30px;
+}
+.circle14 {
+  border-radius:100%;
+  position: absolute;
+  width: 200px;
+  height: 200px;
+  left: 522px;
+  top: 1565px;
+  border: solid 2px black;
+  font-size: 30px;
+}
+.circle15 {
+  border-radius:100%;
+  position: absolute;
+  width: 200px;
+  height: 200px;
+  left: 786px;
+  top: 1565px;
+  border: solid 2px black;
+  font-size: 30px;
+}
+.circle16{
+  border-radius:100%;
+  position: absolute;
+  width: 200px;
+  height: 200px;
+  left: 1053px;
+  top: 1565px;
+  border: solid 2px black;
+  font-size: 30px;
+}
+.back {
+  position: fixed;
+  left:0px;
+  width:100%;
+  top:-510px;
+  
+}
+.img1 {
+  z-index: 999;
+  position: fixed;
+  width: 94px;
+height: 96px;
+left: 2px;
+top: 3px;
+}
+.img2 {
+  position: absolute;
+  width: 1179px;
+  height: 290px;
+  left: 144px;
+  top: 3557px;
+}
+.img3 {
+  position: absolute;
+  left: 144px;
+  top: 3914px;
+  width: 769px;
+height: 290px;}
+
+.img4 {
+  position: absolute;
+  left: 994px;
+  top: 3914px;
+  width: 329px;
+height: 290px;
+}
+.img5 {
+  position: absolute;
+  left: 144px;
+  top: 4271px;
+  width: 531px;
+height: 290px;
+}
+.img6 {
+  position: absolute;
+  left: 757px;
+  top: 4271px;
+  width: 566px;
+height: 290px;
+}
+.img7 {
+  position: absolute;
+  left: 144px;
+  top: 4628px;
+  width: 1179px;
+height: 290px;
+}
+.img8 {
+  position: absolute;
+  left: 144px;
+  top: 4985px;
+ width: 366px;
+height: 299px;
+}
+.img9 {
+  position: absolute;
+  left: 568px;
+  top: 4985px;
+  width: 366px;
+height: 299px;
+}
+.img10 {
+  position: absolute;
+  left: 992px;
+  top: 4985px;
+  width: 331px;
+height: 299px;
+}
+.img11 {
+  position: absolute;
+  left: 144px;
+  top: 5351px;
+  width: 696px;
+height: 299px;
+}
+.img12 {
+  position: absolute;
+  left: 913px;
+  top: 5351px;
+width: 410px;
+height: 299px;
+}
+
+h0, h1, h2, h3, h4 {
+  font-weight: normal;
+}
+h1 {
+  position: absolute;
+  width: 504px;
+  height: 72px;
+  left: 468px;
+  top: 423px;
+
+  font-family: Press Start 2P;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 72px;
+  line-height: 72px;
+  /* identical to box height */
+  text-align: center;
+
+}
+h2 {
+  position: absolute;
+  width: 180px;
+  height: 42px;
+  left: 620px;
+  top: 2109px;
+
+  font-style: normal;
+  font-weight: normal;
+  font-size: 36px;
+  line-height: 42px;
+}
+.h5 {
+  position: absolute;
+  width: 448px;
+  height: 64px;
+  left: 496px;
+  top: 640px;
+
+  font-family: Press Start 2P;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 64px;
+  line-height: 64px;
+  /* identical to box height */
+  text-align: center;
+
+}
+h3 {
+  position: absolute;
+  width: 150px;
+  height: 42px;
+  left: 635px;
+  top: 2375px;
+
+  font-style: normal;
+  font-weight: normal;
+  font-size: 36px;
+  line-height: 42px;
+}
+h4 {
+  position: absolute;
+  width: 288px;
+  height: 42px;
+  left: 566px;
+  top: 3417px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 36px;
+  line-height: 42px;
+  text-align: center;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+  position: absolute;
+  width: 1138px;
+  height: 104px;
+  left: 151px;
+  top: 2189px;
+
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 28px;
+  text-align: center;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+u1 {
+  list-style-type: none;
+  padding: 0;
+  position: absolute;
+  width: 1138px;
+  height: 45px;
+  left: 151px;
+  top: 2455px;
+
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 21px;
+  text-align: center;
+}
+u2 {
+  position: absolute;
+  width: 407px;
+  height: 374px;
+  left: 270px;
+  top: 2564px;
+
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 23px;
+  text-align: center;
+}
+u3{
+  position: absolute;
+  width: 407px;
+  height: 303px;
+  left: 744px;
+  top: 2564px;
+
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 23px;
+  text-align: center;
+}
+u4, u5, u6, u7, u8, u9, u10, u11, u12, u13, u14 {
+  position: absolute;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 16px;
+}
+u4 {
+  width: 474px;
+  height: 16px;
+  left: 144px;
+  top: 3858px;
+}
+u5 {
+  width: 363px;
+  height: 16px;
+  left: 144px;
+  top: 4215px;
+}
+u6 {
+  width: 351px;
+  height: 16px;
+  left: 994px;
+  top: 4215px;
+}
+u7 {
+  width: 358px;
+  height: 16px;
+  left: 144px;
+  top: 4572px;
+}
+u8 {
+  width: 421px;
+  height: 16px;
+  left: 757px;
+  top: 4572px;
+}
+u9 {
+  width: 343px;
+  height: 16px;
+  left: 143px;
+  top: 4929px;
+}
+u10 {
+  width: 253px;
+  height: 18px;
+  left: 144px;
+  top: 5295px;
+}
+u11 {
+  width: 307px;
+  height: 16px;
+  left: 568px;
+  top: 5295px;
+}
+u12 {
+  width: 325px;
+  height: 16px;
+  left: 992px;
+  top: 5292px;
+}
+u13 {
+  width: 505px;
+  height: 16px;
+  left: 90px;
+  top: 5661px;
+}
+u14 {
+  width: 238px;
+  height: 16px;
+  left: 913px;
+  top: 5661px;
+}
+u15, u16, u17, u18, u19, u20, u21, u22, u31, u32, u33, u34, u35, u36, u37, u38 {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 16px;
+}
+u15 {
+  position:fixed;
+  left: 46.65%;
+  right: 47.85%;
+  top: 1.63%;
+  bottom: 98.05%;
+}
+u16 {
+  position:fixed;
+  left: 53.96%;
+  right: 42.05%;
+  top: 1.63%;
+  bottom: 98.05%;
+}
+u17 {
+  position:fixed;
+  left: 60.26%;
+  right: 35.79%;
+  top: 1.63%;
+  bottom: 98.05%;
+}
+u18 {
+  position:fixed;
+  left: 65.89%;
+  right: 28.61%;
+  top: 1.63%;
+  bottom: 98.05%;
+}
+u19 {
+  position:fixed;
+  left: 72.98%;
+  right: 23.02%;
+  top: 1.63%;
+  bottom: 98.05%;
+}
+u20 {
+  position:fixed;
+  left: 78.6%;
+  right: 15.9%;
+  top: 1.63%;
+  bottom: 98.05%;
+}
+u21 {
+  position:fixed;
+  left: 85.06%;
+  right: 9.44%;
+  top: 1.63%;
+  bottom: 98.05%;
+}
+u22 {
+  position:fixed;
+  left: 91.44%;
+  right: 3.06%;
+  top: 1.63%;
+  bottom: 98.05%;
+}
+u23, u24, u25, u26, u27, u28, u29, u30 {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 36px;
+  line-height: 42px;
+}
+u23 {
+  position: absolute;
+  left: 18.47%;
+  right: 68.26%;
+  top: 23.31%;
+  bottom: 75.91%;
+}
+u24 {
+  position: absolute;
+  left: 38.89%;
+  right: 51.6%;
+  top: 23.31%;
+  bottom: 75.91%;
+}
+u25 {
+  position: absolute;
+  left: 57.22%;
+  right: 33.61%;
+  top: 23.31%;
+  bottom: 75.91%;
+}
+u26 {
+  position: absolute;
+  left: 77.01%;
+  right: 16.88%;
+  top: 23.31%;
+  bottom: 75.91%;
+}
+u27 {
+  position: absolute;
+  left: 20.35%;
+  right: 70.49%;
+  top: 27.34%;
+  bottom: 71.88%;
+}
+u28 {
+  position: absolute;
+  left: 40.35%;
+  right: 54.03%;
+  top: 27.34%;
+  bottom: 71.88%;
+}
+u29 {
+  position: absolute;
+  left: 55.76%;
+  right: 32.15%;
+  top: 27.34%;
+  bottom: 71.88%;
+}
+u30 {
+  position: absolute;
+  left: 77.22%;
+  right: 17.15%;
+  top: 27.34%;
+  bottom: 71.88%;
+}
+.under{
+  width:100%;
+  height: 20px;
+  position: relative;
+  top:5800px;
+}
+
+</style>
